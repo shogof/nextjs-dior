d45ce732f54c5d9e9ff5fb583d9b9101fb827a3f
Shegofa <shogofadeveloper12@gmail.com>
2024-10-11 15:37:41 +0400

    SetUp File Structure for Next js app

---
+++ src/app/Collections/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const Collections = () => {
+  return <div></div>;
+};
+
+export default Collections;
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,5 @@
+const Blog = () => {
+  return <div>About Page</div>
+};
+
+export default Blog;
